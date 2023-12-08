13b9f1402d3975785c27e2a6900f610e9c2036ec
Sloth73 <Pichl.tomas.95@gmail.com>
2023-12-08 14:38:24 +0100

    adding bulma library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
-import AlexaImage from './images/alexa.png';
-import CortanaImage from './images/cortana.png';
-import SiriImage from './images/siri.png';
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistant</div>
-      <ProfileCard title="Alexa" handle="@alexa99" image = {AlexaImage} />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
       {/*Adding a props to parent component */}
-          <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage } /> {/* -||- */}
-          <ProfileCard title="Siri" handle="@siri01" image={SiriImage } /> {/* -||- */}
+      <ProfileCard
+        title="Cortana"
+        handle="@cortana32"
+        image={CortanaImage}
+      />{" "}
+      {/* -||- */}
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />{" "}
+      {/* -||- */}
     </div>
   );
 }
